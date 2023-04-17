3f5c0681accdb76ed15297f56783bcc6ae8cd486
bluelotus03 <25125692+bluelotus03@users.noreply.github.com>
2023-04-16 20:11:24 -0400

    beginning animation loops

--- script.js
+++ script.js
@@ -22,15 +22,20 @@ const spriteWidth = 575;
 // Divide height of sprite sheet (5230px) by number of rows (10) to get height of 1 frame --> 523px 
 const spriteHeight = 523;
 
+// Allow us to have movement (move a frame left/right or up/down)
+// X -> travels through sprite sheet horizontally
+let frameX = 0; 
+// Y -> travels through sprite sheet vertically
+let frameY = 0;
+
 
 function animate() { 
-
-    ctx.fillStyle = 'black';
 
     // Clear previous drawing for each animation frame
     // Takes in args to specify what area we want to clear, here we are clearing entire area
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
+    // ctx.fillStyle = 'black';
     // ctx.fillRect(0, 50, 100, 100);
 
     // For working with sprite animations - most interested in canvas drawImage() method
@@ -48,8 +53,21 @@ function animate() {
         // Sixth - Ninth args (canvas destination info) - where on canvas to draw that cropped part of the image (x, y, width, height)
         // ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh);
 
-    
-    ctx.drawImage(playerImage, 0, 0, spriteWidth, spriteHeight, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+    // Multiplying spriteWidth and spriteHeight by a number allows you to use a different frame in that row or column
+        // 0 * the width or height is the first sprite on the sheet
+        // 1 * the width or height is the second sprite on the sheet, and so on
+        // EXAMPLE: 0 * spriteWidth, 0 * spriteHeight is the sprite in first row and column (top left)
+        // EXAMPLE: 0 * spriteWidth, 1 * spriteHeight is the sprite in second row and first column (left and first down)
+        // EXAMPLE: 1 * spriteWidth, 0 * spriteHeight is the sprite in first row and second column 
+    ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+
+    // Check that frameX is less than the number of the last frame in the animation
+    if (frameX < 6) {
+        frameX++;
+    } else {
+        frameX = 0;
+    }
 
     requestAnimationFrame(animate);
 }
